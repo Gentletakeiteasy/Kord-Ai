a56dae626b731854badf8867ccdefc4d4ca65e80
Gentletakeiteasy <ibeabuchi160@gmail.com>
2024-11-05 19:38:48 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -7,22 +7,22 @@ global.settings = {
     MONGO_DB_URL: 'mongodb+srv://miracle32669:Iyanu1234@cluster0.m7glm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     WORK_MODE: "Private",
     READ_ALL_MESSAGES: false,
-    PREFIX: [".", "/", "!"],
+    PREFIX: ["$", "/", "!"],
     WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
     DEFAULT_TRANSLATION_LANG: "en",
-    COMMAND_COOLDOWN_TIME_IN_MS: 2000,
-    MAX_COMMANDS_PER_MINUTE: 10,
-    MAX_DOWNLOAD_SIZE: 100,
+    COMMAND_COOLDOWN_TIME_IN_MS: 200,
+    MAX_COMMANDS_PER_MINUTE: 100,
+    MAX_DOWNLOAD_SIZE: 10000,
     API_KEYS: {
         OPENWEATHERMAP_API_KEY: ''
     },
     isHackEnable:true,
-    SAFE_SEARCH: true,
+    SAFE_SEARCH: false,
     REJECT_CALLS: true,
     SEND_WELCOME_MESSAGE: false,
     WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
     DELETE_WA_LINKS: false,
     BAD_WORD_FILTER: true,
-    BAD_WORDS: ['badword1', 'badword2', 'fuck', 'bitch', 'hentai'],
+    BAD_WORDS: ['badword1', 'badword2', 'fuck', 'dm' . 'bitch', 'hentai'],
     ANTI_DELETE_ENABLED: true // Add this line for anti-delete feature
 };
